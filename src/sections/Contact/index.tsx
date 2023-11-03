82fcdeb8d123eeac3864ddec9e8903184d42f0ed
Paulo Marolla <paulo.marolla@mutual.club>
2023-11-03 00:05:48 -0300

    feat: responsive contact

--- src/sections/Contact/index.tsx
+++ src/sections/Contact/index.tsx
@@ -1,20 +1,83 @@
+import Link from 'next/link'
 import { tv } from 'tailwind-variants'
 
 const contact = tv({
   slots: {
     container: 'bg-primary-500 min-h-screen h-full w-full',
-    content: 'container mx-auto',
-    title: 'text-gray-300 uppercase font-roboto text-[40px] tracking-wide'
+    content: 'container mx-auto items-center min-h-screen h-full px-6 grid',
+    socialContainer: 'md:col-span-4 pt-6 md:pt-0 xl:col-span-10',
+    title: 'md:col-span-2 xl:col-span-2 text-gray-100 uppercase font-gothic text-6xl md:text-[90px] md:tracking-[-1rem] md:[writing-mode:vertical-lr] md:[text-orientation:upright]',
+    ul: '',
+    subtitle: 'text-3xl sm:text-[40px] text-gray-300 uppercase font-roboto tracking-wide pb-6',
+    li: 'w-full max-w-full',
+    a: 'text-2xl sm:text-3xl flex w-full items-center justify-between uppercase text-gray-300 border-gray-300 border-b-[2px] pt-6 pb-4 font-roboto tracking-widest transition-colors hover:text-gray-100 hover:border-gray-100'
   }
 })
 
-const { container, content, title } = contact()
+const links = [
+  {
+    title: 'linkedin',
+    url: 'https://www.linkedin.com/in/paulomarolla/'
+  },
+  {
+    title: 'instagram',
+    url: 'https://www.instagram.com/paulomarolla/'
+  },
+  {
+    title: 'github',
+    url: 'https://github.com/paulomarolla'
+  },
+  {
+    title: 'dribbble',
+    url: 'https://dribbble.com/PauloMarolla'
+  },
+  {
+    title: 'whatsapp',
+    url: 'tel:+5519996489155'
+  },
+  {
+    title: 'e-mail',
+    url: 'mailto:paulo.ricardo.marolla@hotmail.com'
+  },
+]
+
+const { container, content, title, subtitle, li, ul, socialContainer, a } = contact()
 
 export const Contact: React.FC = () => {
   return (
     <section className={container()}>
       <div className={content()}>
-        <h1 className={title()}>vamos conversar</h1>
+        <div className='grid grid-cols-1 md:grid-cols-6 xl:grid-cols-12'>
+       
+          <h1 className={title()}>contato</h1>
+          <div className={socialContainer()}>
+            <h1 className={subtitle()}>vamos conversar</h1>
+            <ul className={ul()}>
+              {links.map((link) => {
+                return (
+                  <li className={li()} key={link.title}>
+                    <Link className={a()} target='_blank' href={link.url}>
+                      <span>{link.title}</span>
+                      <svg width='36' height='39' viewBox='0 0 36 39' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                        <g clipPath='url(#clip0_36_292)'>
+                          <path d='M25.5 11.375L10.5 27.625' stroke='#989898' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'/>
+                          <path d='M12 11.375H25.5V26' stroke='#989898' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'/>
+                        </g>
+                        <defs>
+                          <clipPath id='clip0_36_292'>
+                            <rect width='36' height='39' fill='white'/>
+                          </clipPath>
+                        </defs>
+                      </svg>
+
+                    </Link>
+                  </li>
+                )
+              })}
+         
+            </ul>
+          </div>
+        </div>
       </div>
     </section>
   )
