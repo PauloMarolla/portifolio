21e2d3dd0049d9d3f64b34e397465504b476cd74
Paulo Marolla <paulo.ricardo.marolla@hotmail.com>
2025-03-10 16:49:15 -0300

    fix: build

--- src/hooks/useDevice/useDevice.tsx
+++
@@ -1,8 +0,0 @@
-import { useMediaQuery } from '@uidotdev/usehooks'
-
-export const useDevice = () => {
-  const isMobile = useMediaQuery('only screen and (max-width : 767px)')
-  const isLower840 = useMediaQuery('only screen and (max-width : 840px)')
-
-  return { isMobile, isLower840 }
-}
